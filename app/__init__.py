3741b03568b258bfc7874104b635be4c50613f3a
wkdsh21 <wkdsh21@naver.com>
2023-06-29 04:07:17 +0900

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+    # blueprint
+    from . import main
+
+    app.register_blueprint(main.bp)
+
+    return app
